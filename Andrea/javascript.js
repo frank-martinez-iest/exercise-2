398ba617c42511cd493cd41113c0f8c0faa8eb12
nanyssalazar <andrea.sosa01@iest.edu.mx>
2020-12-15 19:42:58 -0600

    SFO-12: Create navbar, page content, footer, formand make page responsive

---
+++ Andrea/javascript.js
@@ -0,0 +1,8 @@
+function myFunction() {
+  var x = document.getElementById("user-login");
+  if (x.style.display === "none") {
+  x.style.display = "block";
+  } else {
+  x.style.display = "none";
+  }
+}
